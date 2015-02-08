ab93e329e8e8597e91a0a7dd28ae031ace6573b1
Pito Salas <pitosalas@gmail.com>
2015-02-08 17:16:16 -0500

    baseline

---
+++ rental.rb
@@ -0,0 +1,8 @@
+# A rental event
+class Rental
+  attr_reader :movie, :days_rented
+
+  def initialize(movie, days_rented)
+    @movie, @days_rented = movie, days_rented
+  end
+end
---
+++ rental_test.rb
@@ -0,0 +1,21 @@
+require_relative 'movie'
+require_relative 'rental'
+require_relative 'customer'
+require 'minitest/autorun'
+
+describe "Movie rentals" do
+  def setup
+    @c = Customer.new("Jane")
+    @m1 = Movie.new("1984", 0)
+    @m2 = Movie.new("Platoon", 1)
+    @r1 = Rental.new(@m1, 2)
+    @r2 = Rental.new(@m2, 3)
+    @c.add_rental(@r1)
+    @c.add_rental(@r2)
+  end
+
+  it "correctly does statement" do
+    stmt = @c.statement
+    stmt.must_equal("Rental Record for Jane\n\t1984\t2\n\tPlatoon\t9\nAmount owed is 11\nYou earned 3 frequent renter points")
+  end
+end
